a67b540e4792483ea066c03a57c60a71165fc96c
erez <erezt.dev@gmail.com>
2020-05-07 09:49:54 +0300

    init + jquery popper bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="container">
+      <div class="jumbotron bg-">
+        <h1>Bootstrap Tutorial</h1>
+        <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
+          responsive, mobile-first projects on the web.</p>
+      </div>
+      <p>This is some text.</p>
+      <p>This is another text.</p>
+    </div>  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import jQuery from 'jquery'
+global.$ = jQuery
 
 Vue.config.productionTip = false
 
